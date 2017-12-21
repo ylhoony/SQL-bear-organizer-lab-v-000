316b3a371a0c976761e586e7d3fa1575adbe2d34
Hoon Lee <yl.hoony@gmail.com>
2017-12-21 20:37:10 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -2,7 +2,7 @@ CREATE TABLE bears (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER,
-  gender CHAR(1),
+  gender (M VARCHAR(10), F VARCHAR(10))
   color TEXT,
   temperament TEXT,
   alive BOOLEAN
