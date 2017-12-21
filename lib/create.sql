dfeda2e178c376b3defe2210996b73d37b200971
Hoon Lee <yl.hoony@gmail.com>
2017-12-21 20:51:20 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -2,7 +2,7 @@
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER,
-  gender (M VARCHAR(10), F VARCHAR(10)),
+  gender (M VARCHAR(1), F VARCHAR(1)),
   color TEXT,
   temperament TEXT,
   alive BOOLEAN
