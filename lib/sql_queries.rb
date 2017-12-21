144018dfbf140ae9caa6d60caee9a75e1010fb85
Hoon Lee <yl.hoony@gmail.com>
2017-12-21 21:08:57 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,13 +1,13 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT name, age FROM bears WHERE gender == 'F'"
+  "SELECT name, age FROM bears WHERE gender == 'F';"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "SELECT name FROM bears ORDER BY name"
+  "SELECT name FROM bears ORDER BY name;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT name, age FROM bears WHERE alive == 1 ORDER BY age DESC"
+  "SELECT name, age FROM bears WHERE alive == 1 ORDER BY age DESC;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
