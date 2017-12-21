7b6464d856bf56b5bc8394b7b8c7d3fc5a4abda3
Hoon Lee <yl.hoony@gmail.com>
2017-12-21 21:23:28 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color, count(color) FROM bears GROUP BY color LIMIT 1;"
+  "SELECT color, count(color) AS count FROM bears GROUP BY color ORDER BY count DESC LIMIT 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
