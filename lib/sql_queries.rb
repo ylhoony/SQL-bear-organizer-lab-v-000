83bc57b730112aee1ab3c97302a32e6eab30a4b9
Hoon Lee <yl.hoony@gmail.com>
2017-12-21 21:12:21 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color, count(color) FROM bears WHERE color == 'Black';""
+  "SELECT color, count(color) FROM bears WHERE color == 'Black';"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
