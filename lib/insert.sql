0900ef3c47bfd1a6454c5664bc315bcc4e544f48
Hoon Lee <yl.hoony@gmail.com>
2017-12-21 21:01:23 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 12, "M", "Dark Brown", "Fire", 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 5, "M", "Light Brown", "Fire", 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 7, "F", "Black", "Fire", 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 11, "M", "Azure", "Fire", 0)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 15, "F", "Black", "Fire", 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 33, "M", "White", "Fire", 0)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy',23, "F", "White", "Fire", 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 3, "F", "Red", "Fire", 1)
